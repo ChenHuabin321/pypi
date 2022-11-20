d4323a9740f5ff4e061d94129fb2c4974e3a4291
CHB <chenhuabin321@163.com>
2022-11-20 23:05:12 +0800

    优化惰性导入代码

--- chb/_imports.py
+++ chb/_imports.py
@@ -1,94 +1,5 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
-
-# 注：此部分代码修改自pyforest
-
-imported = set()
-
-class LazyImport(object):
-    def __init__(self, import_statement):
-        self.__import_statement__ = import_statement
-        # the next line does not work for general imports, e.g. "from pandas import *"
-        self.__imported_name__ = import_statement.strip().split()[-1]
-
-        self.__complementary_imports__ = []
-
-    def __maybe_import_complementary_imports__(self):
-        for lazy_import in self.__complementary_imports__:
-            try:
-                lazy_import.__maybe_import__()
-            except:
-                # silently fails if the complementary lazy_import is not available.
-                # This is because complementary lazy_imports are considered optional.
-                # Please note that direct imports of lazy_imports will fail explicitly.
-                pass
-
-    # Python will only import the module(s) if they are missing
-    # if the module(s) were imported before, this method returns immediately
-    def __maybe_import__(self):
-
-        self.__maybe_import_complementary_imports__()
-        exec(self.__import_statement__, globals())
-        # Attention: if the import fails, the next lines will not be reached
-        self.__was_imported__ = True
-        imported.add(self.__import_statement__)
-        self.__maybe_add_docstring_and_signature__()
-
-    def __maybe_add_docstring_and_signature__(self):
-        # adds docstrings for imported objects
-        # UnitRegistry = LazyImport("from pint import UnitRegistry")
-        # UnitRegistry?
-
-        try:
-            self.__doc__ = eval(f"{self.__imported_name__}.__doc__")
-
-            from inspect import signature
-
-            self.__signature__ = eval(f"signature({self.__imported_name__})")
-        except:
-            pass
-
-    # among others, called during auto-completion of IPython/Jupyter
-    def __dir__(self):
-        self.__maybe_import__()
-        return eval(f"dir({self.__imported_name__})")
-
-    # called for undefined attribute and returns the attribute of the imported module
-    def __getattr__(self, attribute):
-        self.__maybe_import__()
-        return eval(f"{self.__imported_name__}.{attribute}")
-
-    # called for callable objects, e.g. from pathlib import Path; Path(".")
-    def __call__(self, *args, **kwargs):
-        self.__maybe_import__()
-        return eval(self.__imported_name__)(*args, **kwargs)
-
-    def __repr__(self, *args, **kwargs):
-        # it is important that __repr__ does not trigger an import if the lazy_import is not yet imported
-        # e.g. if the user calls locals(), this triggers __repr__ for each object
-        # and this would result in an import if the lazy_import is not yet imported
-        # and those imports might fail explicitly if the lazy_import is not available
-        # and this would break locals() for the user
-
-        if self.__was_imported__:
-            # next line only adds imported_name into the local scope but does not trigger a new import
-            # because the lazy_import was already imported via another trigger
-            self.__maybe_import__()
-            return f"active chb.LazyImport of {eval(self.__imported_name__)}"
-        else:
-            return f"lazy chb.LazyImport for '{self.__import_statement__}'"
-
-
-def _get_import_statements(symbol_dict, was_imported=True):
-    statements = []
-    for _, symbol in symbol_dict.items():
-        if isinstance(symbol, LazyImport) and (symbol.__was_imported__ == was_imported):
-            statements.append(symbol.__import_statement__)
-
-    # remove potential duplicates, e.g. when user_symbols are passed
-    statements = list(set(statements))
-    return statements
-
+from ._importable import LazyImport, _get_import_statements
 
 ### Data Wrangling
 pd = LazyImport("import pandas as pd")
@@ -114,13 +25,13 @@ py = LazyImport("import plotly as py")
 go = LazyImport("import plotly.graph_objs as go")
 px = LazyImport("import plotly.express as px")
 
-dash = LazyImport("import dash")
+# dash = LazyImport("import dash")
 
-bokeh = LazyImport("import bokeh")
+# bokeh = LazyImport("import bokeh")
 
-alt = LazyImport("import altair as alt")
+# alt = LazyImport("import altair as alt")
 
-pydot = LazyImport("import pydot")
+# pydot = LazyImport("import pydot")
 
 ### Image processing
 
@@ -134,10 +45,10 @@ statistics = LazyImport("import statistics")
 stats = LazyImport("from scipy import stats")
 sm = LazyImport("import statsmodels.api as sm")
 
-### Time-Series Forecasting
-fbprophet = LazyImport("import fbprophet")
-Prophet = LazyImport("from fbprophet import Prophet")
-ARIMA = LazyImport("from statsmodels.tsa.arima_model import ARIMA")
+# ### Time-Series Forecasting
+# fbprophet = LazyImport("import fbprophet")
+# Prophet = LazyImport("from fbprophet import Prophet")
+# ARIMA = LazyImport("from statsmodels.tsa.arima_model import ARIMA")
 
 ### Machine Learning
 sklearn = LazyImport("import sklearn")
@@ -207,6 +118,22 @@ tf = LazyImport("import tensorflow as tf")
 keras = LazyImport("import keras")
 torch = LazyImport("import torch")
 fastai = LazyImport("import fastai")
+nn = LazyImport("import torch.nn as nn")
+F = LazyImport("import torch.nn.functional as F")
+optim = LazyImport("import torch.optim as optim")
+lr_scheduler = LazyImport("from torch.optim import lr_scheduler")
+cudnn = LazyImport("import torch.backends.cudnn as cudnn")
+torchvision = LazyImport("import torchvision")
+datasets = LazyImport("from torchvision import datasets")
+models = LazyImport("from torchvision import models")
+Conv2d = LazyImport("from torch.nn import Conv2d")
+MaxPool2d = LazyImport("from torch.nn import MaxPool2d")
+Flatten = LazyImport("from torch.nn import Flatten")
+Linear = LazyImport("from torch.nn import Linear")
+Sequential = LazyImport("from torch.nn import Sequential")
+DataLoader = LazyImport("from torch.utils.data import DataLoader")
+transforms = LazyImport("from torchvision import transforms")
+trange = LazyImport("from tqdm import trange")
 
 # NLP
 nltk = LazyImport("import nltk")
@@ -221,17 +148,18 @@ AutoTokenizer = LazyImport("from transformers import AutoTokenizer")
 BertConfig = LazyImport("from transformers import BertConfig")
 
 ### Helper
-sys = LazyImport("import sys")
 os = LazyImport("import os")
-random = LazyImport("import random")
 time = LazyImport("import time")
+sys = LazyImport("import sys")
+random = LazyImport("import random")
 glob = LazyImport("import glob")
 logging = LazyImport("import logging")
 Path = LazyImport("from pathlib import Path")
-
 pickle = LazyImport("import pickle")
+json = LazyImport("import json")
 
 dt = LazyImport("import datetime as dt")
+datetime = LazyImport("import datetime")
 
 tqdm = LazyImport("import tqdm")
 
@@ -248,18 +176,27 @@ Process = LazyImport("from multiprocessing import Process")
 multiprocessing = LazyImport("import multiprocessing import Process")
 queue = LazyImport("import queue")
 
-MongoDao = LazyImport("from ._dao import MongoDao")
-OracleDao = LazyImport("from ._dao import OracleDao")
-MysqlDao = LazyImport("from ._dao import MysqlDao")
-RedisDao = LazyImport("from ._dao import RedisDao")
-Log = LazyImport("from ._log import Log")
-get_current_path = LazyImport("from ._utils import get_current_path")
-get_time_str = LazyImport("from ._utils import get_time_str")
-MutilThreadReader = LazyImport("from ._utils import MutilThreadReader")
-Tableprint = LazyImport("from ._utils import Tableprint")
+MongoDao = LazyImport("from chb._dao import MongoDao")
+OracleDao = LazyImport("from chb._dao import OracleDao")
+MysqlDao = LazyImport("from chb._dao import MysqlDao")
+RedisDao = LazyImport("from chb._dao import RedisDao")
+Log = LazyImport("from chb._log import Log")
+get_current_path = LazyImport("from chb._utils import get_current_path")
+get_time_str = LazyImport("from chb._utils import get_time_str")
+MutilThreadReader = LazyImport("from chb._utils import MutilThreadReader")
+Tableprint = LazyImport("from chb._utils import Tableprint")
 
-def lazy_imports():
+def all_import():
+    """所有导入语句"""
+    return _get_import_statements(globals(), was_imported=None)
+
+def unimport():
+    """所有未执行导入的语句"""
     return _get_import_statements(globals(), was_imported=False)
 
-def active_imports():
-    return imported
+def imported(print_statements=True):
+    """所有已经执行导入的语句"""
+    statements = _get_import_statements(globals(), was_imported=True)
+    if print_statements:
+        print("\n".join(statements))
+    return statements
